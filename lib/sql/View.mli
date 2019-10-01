44f53564723a53f6ebbccc77d3bbe415e41496e9
Calvin Smith <calvin@Calvins-MacBook-Air.local>
2019-10-01 10:55:40 -0500

    cleanup

--- lib/sql/View.mli
+++ lib/sql/View.mli
@@ -2,5 +2,5 @@ type t
 
 val of_json : Yojson.Basic.t -> t option
 
-val attributes : t -> Core.Thing.attribute list
-val relations : t -> Core.Scene.relation_key list
+val attributes : t -> Core.Store.relation_key list
+val relations : t -> Core.Store.relation_key list
